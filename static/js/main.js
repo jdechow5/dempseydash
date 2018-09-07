5fe607725c1e9061c108323ff1b9023156604bdc
Vinh Nguyen <meximese@gmail.com>
2018-09-07 16:21:46 -0700

    first stab at site with parallax

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+console.log('main.js');
+// Also can pass in optional settings block
+var rellax = new Rellax('.rellax', {
+  speed: -2,
+  // center: false,
+  // wrapper: '.wrapper',
+  // round: true,
+  // vertical: true,
+  // horizontal: false
+});
